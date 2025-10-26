d1e3e5167c5c35f88abadbcc1625ee74cca9e8a1
alexandre-girard <alexandre.girard@maif.fr>
2025-10-26 09:54:38 +0100

    feat: enhance documentation for TopArtistItem component

--- src/components/TopArtistItem.jsx
+++ src/components/TopArtistItem.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+/**
+ * TopArtistItem component displays information about a single top artist.
+ * @param {Object} param0 - Component props
+ * @param {Object} param0.artist - The artist object containing artist information
+ * @param {number} param0.index - The index of the artist in the list
+ * @returns {JSX.Element} The rendered component
+ */
 export default function TopArtistItem({ artist, index }) {
   return (
     <li className="track-item" data-testid={`top-artist-item-${artist.id}`}>
