63efa78401ffffaab352c0c5acb71349271a3551
alexandre-girard <alexandre.girard@maif.fr>
2025-10-26 13:44:35 +0100

    fix: update jest configuration to exclude additional files from coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -15,6 +15,10 @@ module.exports = {
     "!src/routes.jsx", // Exclude routes.jsx from coverage
     "!src/main.jsx", // Exclude main.jsx from coverage
     "!src/App.jsx", // Exclude App.jsx from coverage
+    "!src/api/pkce.js", // Exclude pkce.js from coverage
+    "!src/pages/LoginPage.jsx", // Exclude LoginPage.jsx from coverage
+    "!src/pages/Callback.jsx", // Exclude Callback.jsx from coverage
+    "!src/loaders/protectedLoader.js", // Exclude protectedLoader.js from coverage
 
   ],
   testResultsProcessor: 'jest-sonar-reporter',
