61dbc6942e342112f9f3d83b79ec8d0c7aafb9e9
alexandre-girard <alexandre.girard@maif.fr>
2025-10-26 05:30:20 +0100

    Add testing setup and TrackItem component tests
    - Updated package.json to include Jest and testing libraries.
    - Created a new test file for the TrackItem component with a test case to verify rendering of track information.

---
+++ jest.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  testEnvironment: "jest-fixed-jsdom", // Use jsdom environment for testing React components
+  // Transform jsx files using babel-jest
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+
+  // ignore e2e tests
+  testPathIgnorePatterns: [
+    "/e2e-tests/",
+  ],
+  
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx}", // Collect coverage from all js or jsx files in src folder
+    "!src/routes.js", // Exclude routes.js from coverage
+    "!src/main.jsx", // Exclude main.jsx from coverage
+    "!src/App.jsx", // Exclude App.jsx from coverage
+
+  ],
+  testResultsProcessor: 'jest-sonar-reporter',
+  moduleNameMapper: {
+    "d3": "<rootDir>/node_modules/d3/dist/d3.min.js"
+  }
+};
