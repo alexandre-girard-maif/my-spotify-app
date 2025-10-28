6423bd5d682720981ea8e19e29378acc26f713b9
alexandre-girard <alexandre.girard@maif.fr>
2025-10-28 06:48:02 +0100

    feat: remove unused links from WelcomePage and update tests accordingly

--- src/pages/WelcomePage.test.jsx
+++ src/pages/WelcomePage.test.jsx
@@ -2,7 +2,7 @@
 
 import { describe, expect, test } from '@jest/globals';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { createRoutesStub } from 'react-router-dom';
 
 import WelcomePage from './WelcomePage.jsx';
@@ -25,18 +25,5 @@ describe('WelcomePage', () => {
 
         // verify document title is set
         expect(document.title).toBe('Welcome | Spotify App');
-
-        // verify links
-        const topTracksLink = screen.getByText('Top Tracks');
-        expect(topTracksLink).toBeInTheDocument();
-        expect(topTracksLink.closest('a')).toHaveAttribute('href', '/top-tracks');
-
-        const topArtistsLink = screen.getByText('Top Artists');
-        expect(topArtistsLink).toBeInTheDocument();
-        expect(topArtistsLink.closest('a')).toHaveAttribute('href', '/top-artists');
-
-        const playlistsLink = screen.getByText('Playlists');
-        expect(playlistsLink).toBeInTheDocument();
-        expect(playlistsLink.closest('a')).toHaveAttribute('href', '/playlists');
     });
 });
