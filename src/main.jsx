f3f7d32e08043281cabeaf5ace174295d83f429f
alexandre-girard <alexandre.girard@maif.fr>
2025-11-07 06:49:22 +0100

    refactor: remove unused WelcomePage styles import from main entry

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/theme.css';
 import './styles/index.css';
-import './pages/WelcomePage/WelcomePage.css'; // if global-ish
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
