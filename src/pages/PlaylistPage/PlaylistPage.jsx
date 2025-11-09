820ef129a49e9cbfb47d59e5abe46c0d2888fead
alexandre-girard <alexandre.girard@maif.fr>
2025-11-09 15:40:59 +0100

    feat: implement fetching of playlist data using playlist ID and token

--- src/pages/PlaylistPage/PlaylistPage.jsx
+++ src/pages/PlaylistPage/PlaylistPage.jsx
@@ -1,11 +1,44 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useRequireToken } from "../../hooks/useRequireToken";
+import { fetchPlaylistById } from "../../api/spotify-playlists";
+import { handleTokenError } from "../../utils/handleTokenError";
 
 /**
  * Playlist Page
  * @returns {JSX.Element}
  */
 export default function PlaylistPage() {
+    // Initialize navigate function
+    const navigate = useNavigate();
+
     // retrieve id from params
     const { id } = useParams();
+
+    // require token to fetch top tracks
+    const { token } = useRequireToken();
+
+    // state for loading and error
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        if (!token) return; // wait for check or redirect
+
+        // fetch playlist data using id and token
+        fetchPlaylistById(token, id)
+            .then(res => {
+                if (res.error) {
+                    if (!handleTokenError(res.error, navigate)) {
+                        setError(res.error);
+                    }
+                }
+                console.log(res);
+            })
+            .catch(err => { setError(err.message); })
+            .finally(() => { setLoading(false); });
+    }, [id,token, navigate]);
+
     return <div>Playlist Page {id}</div>;
 }
